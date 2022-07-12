73a54de54d94841295303abaebbe06dd950d3a0e
superflyman90 <superflyman90@gmail.com>
2022-07-12 20:22:25 +0200

    ajout mongoose sur server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import routes from './routes/routes.js';
 dotenv.config();
@@ -7,8 +8,15 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-app.use(routes)
 app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+}
+
+app.use(routes)
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
