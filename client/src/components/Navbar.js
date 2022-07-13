5e10e06851f1168c30266b8ce4aed63f0a3f523c
superflyman90 <superflyman90@gmail.com>
2022-07-13 10:44:44 +0200

    Ajout scss de la navbar

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <h1 className='title'>Nephis Airline</h1>
+            <nav className="navbar_menu">
+                <ul className="navbar_menu_list">
+                    <li className="navbar_menu_item">
+                        <NavLink to='/'>Home</NavLink>
+                    </li>
+                    <li className="navbar_menu_item">
+                        <NavLink to='/pilotes'>Pilotes</NavLink>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    );
+};
+
+export default Navbar;
---
+++ client/src/pages/Pilotes.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Pilotes = () => {
+    return (
+        <div>
+            <h2>Nos pilotes</h2>
+        </div>
+    );
+};
+
+export default Pilotes;
